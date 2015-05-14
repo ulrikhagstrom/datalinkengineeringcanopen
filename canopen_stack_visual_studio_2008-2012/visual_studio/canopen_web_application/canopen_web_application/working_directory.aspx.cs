6317fa5907a995c72afbeab1a5521cf7a0d2f5b2
ulrikhagstrom <ulrikhagstrom@2a4e5ae4-3336-458a-b986-d48fbabd4c73>
2015-05-14 12:50:44 +0000

    Initial implementation of web-demo.

---
+++ canopen_stack_visual_studio_2008-2012/visual_studio/canopen_web_application/canopen_web_application/working_directory.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace canopen_web_application
+{
+    public partial class working_directory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.Response.Write(System.IO.Directory.GetCurrentDirectory());
+
+        }
+    }
+}
