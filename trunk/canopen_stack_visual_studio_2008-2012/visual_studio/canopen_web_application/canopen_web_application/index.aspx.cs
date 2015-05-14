809f36ae2e40b802ef066e58dbff603a8a1974b6
ulrikhagstrom <ulrikhagstrom@2a4e5ae4-3336-458a-b986-d48fbabd4c73>
2015-05-14 14:07:32 +0000

    Code has started to work.

--- trunk/canopen_stack_visual_studio_2008-2012/visual_studio/canopen_web_application/canopen_web_application/index.aspx.cs
+++ trunk/canopen_stack_visual_studio_2008-2012/visual_studio/canopen_web_application/canopen_web_application/index.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Text;
+using System.Timers;
 
 namespace canopen_web_application
 {
@@ -21,7 +22,7 @@ namespace canopen_web_application
         private static NMT_Master_NET nmt_Master;
         private static CanMonitor_NET can_monitor;
 
-        private static IList<CanFrame> frames = new List<CanFrame>();
+        private static Queue<CanFrame> frames = new Queue<CanFrame>();
 
         static CANOPEN_LIB_ERROR.CanOpenStatus canReceiveCallback(object obj, uint id, byte[] data, byte dlc, uint flags)
         {
@@ -39,13 +40,40 @@ namespace canopen_web_application
                 }
             }
 
-            frames.Add(new CanFrame() { Id = id.ToString(), Data = tempString.ToString(), Dlc = dlc.ToString() });
+            lock (frames)
+            {
+                frames.Enqueue(new CanFrame() { Id = id.ToString(), Data = tempString.ToString(), Dlc = dlc.ToString() });
+
+                if (frames.Count() > 10)
+                    frames.Dequeue();
+            }
 
             return CANOPEN_LIB_ERROR.CanOpenStatus.CANOPEN_OK;
         }
 
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            Label1.Text = "Panel refreshed at: " +
+            DateTime.Now.ToLongTimeString();
+
+            lock (frames)
+            {
+                CanGrid.DataSource = frames;
+                CanGrid.DataBind();
+            }
+        }
+
+        public void UpdateLabel(string text)
+        {
+            CanGrid.DataBind();
+
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack == true)
+                return;
+
             CANOPEN_LIB_ERROR.CanOpenStatus stat;
 
             if (client_SDO == null || nmt_Master == null || can_monitor == null)
@@ -66,11 +94,15 @@ namespace canopen_web_application
                 if (stat != CANOPEN_LIB_ERROR.CanOpenStatus.CANOPEN_OK)
                 {
                 }
+                stat = client_SDO.connect(3);
 
                 stat = nmt_Master.canHardwareConnect(0, 125000);
                 if (stat != CANOPEN_LIB_ERROR.CanOpenStatus.CANOPEN_OK)
                 {
                 }
+
+                CanGrid.DataSource = frames;
+
             }
 
             
@@ -79,6 +111,21 @@ namespace canopen_web_application
         protected void btnStartNodeGuardNode3_Click(object sender, EventArgs e)
         {
             nmt_Master.nodeGuardPollStart(3, 1000);
+        }
+
+
+
+        protected void btnStopNodeGuardNode3_Click(object sender, EventArgs e)
+        {
+            nmt_Master.nodeGuardPollStop(3);
+        }
+
+        protected void btnReadDeviceName_Click(object sender, EventArgs e)
+        {
+            byte[] receive_buffer = new byte[100];
+            UInt32 error_code;
+            uint valid;
+            client_SDO.objectRead(0x1008, 0, receive_buffer, out valid, out error_code);
         }
     }
 }
