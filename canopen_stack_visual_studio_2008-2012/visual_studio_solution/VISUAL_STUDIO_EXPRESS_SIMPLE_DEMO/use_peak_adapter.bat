6455f60afe66d26f538e59e0269020274b978b30
Ulrik Hagström <ulrik.hagstrom@datalink.se>
2017-04-09 10:10:01 +0200

    The PCAN-Basic DLL was not reachable after installation.

--- canopen_stack_visual_studio_2008-2012/visual_studio_solution/VISUAL_STUDIO_EXPRESS_SIMPLE_DEMO/use_peak_adapter.bat
+++ canopen_stack_visual_studio_2008-2012/visual_studio_solution/VISUAL_STUDIO_EXPRESS_SIMPLE_DEMO/use_peak_adapter.bat
@@ -1,2 +1,3 @@
 copy ..\..\visual_studio\bin\Release\zip\dlls\peak\canopenlib32_hw.dll ..\..\visual_studio\bin\Debug\canopenlib32_hw.dll
 copy ..\..\visual_studio\bin\Release\zip\dlls\peak\canopenlib32_hw.dll ..\..\visual_studio\bin\Release\canopenlib32_hw.dll
+copy ..\..\visual_studio\plug_in_peak\lib\PCANBasic.dll ..\..\visual_studio\bin\Release\PCANBasic.dll
